110a57aa25952e41caa2f926013c16116f57ebb0
Andrea <andrea.troianiello.94@gmail.com>
2017-05-22 16:02:54 +0200

    add server creation

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+let serverPort = process.env.PORT || 5000;
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("port", serverPort);
+/* Start the server on port 5000 */
+app.listen(serverPort, function() {
+  console.log(`Your app is ready at port ${serverPort}`);
+});
