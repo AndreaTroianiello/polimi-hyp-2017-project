829533dfdd6ccdaa566f28e4b7ed2058f1985437
Andrea <andrea.troianiello.94@gmail.com>
2017-05-24 21:23:18 +0200

    Add simple doctor page

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
+//Call the packages
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-let serverPort = process.env.PORT || 5000;
+const bodyParser = require("body-parser"); 
+const _ = require("lodash");
+
+let doctorsList = require("./other/doctor.json");
+
+let doctor;
+
 app.use(express.static(__dirname + "/public"));
+
+//Allow to get data from a POST
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set("port", serverPort);
-/* Start the server on port 5000 */
+
+//Set the server's port
+let serverPort = process.env.PORT || 5000;
+app.set("port", serverPort); 
+
+
+app.get("/doctor", function(req, res) {
+	let idn = parseInt(_.get(req, "query.id", 0));
+	if(idn>=0 && idn<doctorsList.length)
+  		doctor=_.find(doctorsList, [ 'id' , idn ]);
+  	res.send(JSON.stringify(doctor));
+});
+
+//Start the server on port 5000
 app.listen(serverPort, function() {
   console.log(`Your app is ready at port ${serverPort}`);
 });
