076a21e196abad55ed36ef84ea6bce90b6dd8135
Matteo Colombo <matto32.colombo@mail.polimi.it>
2017-06-01 12:06:19 +0200

    implement next and previous doctor w fitlers

--- public/assets/js/doctor.js
+++ public/assets/js/doctor.js
@@ -1,58 +1,17 @@
 var server = "../"
 var api = "doctors/";
-
+var next;
+var previous;
 
 $(document).ready(function () {
-    $.ajax({
-        method: "GET",
-        dataType: "json",
-        crossDomain: true,
-        url: server + api + URL.id,
-        success: function (response) {
-            $('#id').text(URL.id);
-            $('#curriculum').attr("href","./curriculum.html?id="+URL.id);
-            $('title').text(getDoctorName(response));
-            $('#title-doc-name').text(getDoctorName(response));
-            $('#phone').text(response.phone);
-            $('#phone').attr("href", "tel:" + response.phone);
-            $('#fax').text(response.fax);
-            $('#fax').attr("href", "tel:" + response.fax);
-            $('#email').text(response.email);
-            $('#email').attr("href", "mailto:" + response.email);
-            $('.doc-image').attr("src", response.img);
-            $('.doc-info').text(response.desc);
-            var op = "<a href='./service.html?id=" + response.operates + "'>" + getServiceName(response.operates) + "</a>";
-            $('#operates').html(op);
-            var m_a = "-";
-            if (response.manages_a !== null) {
-                m_a = "<a href='./area.html?id=" + response.manages_a + "'>" + getAreaName(response.manages_a) + "</a>";
-            }
-            $('#manages_a').html(m_a);
-            var m_s = "-";
-            if (response.manages_s !== null) {
-                m_s = "<a href='./service.html?id=" + response.manages_s + "'>" + getServiceName(response.manages_s) + "</a>";
-            }
-            $('#manages_s').html(m_s);
-        },
-        error: function (request, error) {
-        }
-    });
 
-    function capitalizeFirstLetter(temp) {
-        return temp.charAt(0).toUpperCase() + temp.slice(1);
+    if (URL.id == null) {
+        URL.id = 0;
     }
+    
+    getDoctor();
 
-    function getDoctorName(obj) {
-        return "Dr." + (obj.male ? " " : "ssa ") + obj.surname + " " + obj.name;
-    }
-
-    function getServiceName(id) {
-        return "Servizio " + id;
-    }
-    function getAreaName(id) {
-        return "Area " + id;
-    }
-
+    
 });
 
 var URL = function () {
@@ -77,3 +36,98 @@ var URL = function () {
     }
     return query_string;
 }();
+
+
+function getDoctor(){
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        crossDomain: true,
+        url: server + api + URL.id,
+        success: function (response) {
+            $('#id').text(URL.id);
+            $('#curriculum').attr("href", "./curriculum.html?id=" + URL.id);
+            $('title').text(getDoctorName(response));
+            $('#title-doc-name').text(getDoctorName(response));
+            $('#phone').text(response.phone);
+            $('#phone').attr("href", "tel:" + response.phone);
+            $('#fax').text(response.fax);
+            $('#fax').attr("href", "tel:" + response.fax);
+            $('#email').text(response.email);
+            $('#email').attr("href", "mailto:" + response.email);
+            $('.doc-image').attr("src", response.img);
+            $('.doc-info').text(response.desc);
+            var op = "<a href='./service.html?id=" + response.operates + "'>" + getServiceName(response.operates) + "</a>";
+            $('#operates').html(op);
+            var m_a = "-";
+            if (response.manages_a !== null) {
+                m_a = "<a href='./area.html?id=" + response.manages_a + "'>" + getAreaName(response.manages_a) + "</a>";
+            }
+            $('#manages_a').html(m_a);
+            var m_s = "-";
+            if (response.manages_s !== null) {
+                m_s = "<a href='./service.html?id=" + response.manages_s + "'>" + getServiceName(response.manages_s) + "</a>";
+            }
+            $('#manages_s').html(m_s);
+            setNext();
+            setPrevious();
+        },
+        error: function (request, error) {
+        }
+    });
+}
+
+function capitalizeFirstLetter(temp) {
+        return temp.charAt(0).toUpperCase() + temp.slice(1);
+    }
+
+    function getDoctorName(obj) {
+        return "Dr." + (obj.male ? " " : "ssa ") + obj.surname + " " + obj.name;
+    }
+
+    function getServiceName(id) {
+        return "Servizio " + id;
+    }
+    function getAreaName(id) {
+        return "Area " + id;
+    }
+
+    function setNext() {
+        $.ajax({
+            method: "GET",
+            dataType: "json",
+            crossDomain: true,
+            url: server + api + URL.id+"/next",
+            data: {
+                "filter": URL.filter,
+                "value": URL.value
+            },
+            success: function (response) {
+                console.log("got "+response.id);
+                $('.nexthref').attr("href","./doctor.html?id="+response.id);
+            },
+            error: function (request, error) {
+
+            }
+        });
+    }
+
+    function setPrevious() {
+        $.ajax({
+            method: "GET",
+            dataType: "json",
+            crossDomain: true,
+            url: server + api + URL.id+"/previous",
+            data: {
+                "filter": URL.filter,
+                "value": URL.value
+            },
+            success: function (response) {
+                console.log("got "+response.id);
+                $('.previoushref').attr("href","./doctor.html?id="+response.id);
+            },
+            error: function (request, error) {
+
+            }
+        });
+    }
