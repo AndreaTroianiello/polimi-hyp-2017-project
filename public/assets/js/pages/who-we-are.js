8ea2b996f8215ac38bd6df9676a15efbb439d1ed
Andrea <andrea.troianiello.94@gmail.com>
2017-06-02 17:05:05 +0200

    Add who we are page

---
+++ public/assets/js/pages/who-we-are.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	console.log("I'm ready");
+	$.ajax({
+		method: "GET",
+		dataType: "json",
+		crossDomain: true,
+		url: "../aboutus",
+		success: function (response) {
+			console.log(response);
+			updateInfo(response);
+		},
+		error: function (request, error) {
+			console.log(request + ":" + error);
+			$('#aboutus-info').empty();
+			errorMessage();
+		}
+	});
+});
+
+function updateInfo(info) {
+	$('#accordion').empty();
+	for(var i=0;i<info.length;++i){
+		$('#accordion').append('<div class="panel panel-default"><div class="panel-heading"><h4 class="panel-title"><a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#'+i+'">'+info[i].tag+'</a></h4></div><div id="'+i+'"class="panel-collapse collapse"><div class="panel-body"><p>'+info[i].infomation+'</p></div></div></div>');
+	}
+}
+
+function errorMessage() {
+	$('#aboutus-info').append('<h3 class="error">Impossibile le informazioni.</h3>');
+}
