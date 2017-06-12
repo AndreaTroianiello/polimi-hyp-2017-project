7e5df37c16ac11deb8c85c90e126e94549c2e580
Matteo Colombo <matteo32.colombo@mail.polimi.it>
2017-06-13 00:23:20 +0200

    RESTful API doc and bugfix

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const _ = require("lodash");
 const process = require("process");
 const dbms = require("./other/database.js");
 const app = express();
-const DEV = process.env.DEV;
+const DEV = process.env.TEST;
 
+//static file directory
 app.use(express.static(__dirname + "/public"));
 
-//Allow to get data from a POST
+//Allows to get data from a POST
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
@@ -25,15 +26,31 @@ let sqlDb = dbms.getSQLDB(DEV);
 //create DB if it doesn't exist
 dbms.initDB();
 
-//Start the server 
+// Start the server 
 app.listen(serverPort, function () {
-	console.log(`Your app is ready at ${serverUrl}`);
+	console.log(`Your app is ready at ${DEV?serverURL[0]:serverURL[1]}`);
 });
+
+/*Converts a relative path to an absolute path
+This method is used to generate absolute URI for the RESTful APIs */
+function makeURLsAbsolute(path, img) {
+	let URL = (DEV ? serverUrl[0] : serverUrl[1]) + (img ? "/assets/img/" : "");
+	return URL + path;
+}
+
 
 /* =========================================================
  Doctors APIs
 ========================================================== */
 
+/* Returns a JSON composed by an array of objects 
+Each object is a distinct Doctor and contains all the characteristic parameters.
+Filtering is supported, parameters:
+- filter: the type of the filter, it can be equal to area|location|service
+- value: the ID of an 'object' of the same type specified by the filter parameter 
+		(e.g the ID of the service you may want to know which doctors operates in)
+Doctor are alphabetically ordered by surname and name.
+The function "filterDoctor" is used for filtering */
 app.get("/doctors", function (req, res) {
 	let query = sqlDb("doctors");
 	filterDoctor(req, query);
@@ -45,6 +62,8 @@ app.get("/doctors", function (req, res) {
 		});
 });
 
+/* Returns a JSON object that corresponds to the doctor specified by "doctor_id" and all his information.
+If the doctor ID is invalid, a JSON object containing an error message is returned. */
 app.get("/doctors/:doctor_id", function (req, res) {
 	sqlDb("doctors").where("id", req.params.doctor_id).then(result => {
 		if (result.length === 0) {
@@ -58,34 +77,33 @@ app.get("/doctors/:doctor_id", function (req, res) {
 	});
 });
 
+/* Returns a JSON object that corresponds to the following doctor to the one specified by "doctor_id" and all his information.
+If the doctor ID is invalid, a JSON object containing an error message is returned.
+Eventual filters are supported and they work in the same way as in the doctors API. */
 app.get("/doctors/:doctor_id/next", function (req, res) {
 	getPrevNext(req, res, true);
 });
 
+/* Returns a JSON object that corresponds to the previous doctor to the one specified by "doctor_id" and all his information.
+If the doctor ID is invalid, a JSON object containing an error message is returned.
+Eventual filters are supported and they work in the same way as in the doctors API. */
 app.get("/doctors/:doctor_id/previous", function (req, res) {
 	getPrevNext(req, res, false);
 });
 
-function getPrevNext(req, res, next) {
-	let query = sqlDb("doctors");
-	query = filterDoctor(req, query);
-	query.orderBy("surname", "asc").orderBy("name", "asc")
-		.then(result => {
-			let i = _.findIndex(result, (r) => {
-				return req.params.doctor_id == r.id;
+/* Returns a JSON object that corresponds to the curriculum information of the specified doctor.
+If the doctor ID is invalid, a JSON object containing an error message is returned. */
+app.get("/doctors/:doctor_id/curriculum", function (req, res) {
+	sqlDb("curriculums").where("doctor", req.params.doctor_id).then(result => {
+		if (result.length === 0) {
+			res.json({
+				error: "Invalid doctor ID"
 			});
-			if (i === -1) {
-				res.json({
-					error: "No doctor found for the given parameters."
-				});
-			} else {
-				let diff = next ? 1 : -1;
-				let doc = result[(i + result.length + diff) % result.length];
-				doc.img = makeURLsAbsolute(doc.img, true);
-				res.json(doc);
-			}
-		});
-}
+		} else {
+			res.json(result[0]);
+		}
+	});
+});
 
 function filterDoctor(req, query) {
 	let filter = _.get(req, "query.filter", null);
@@ -112,22 +130,38 @@ function filterDoctor(req, query) {
 	return query;
 }
 
-app.get("/doctors/:doctor_id/curriculum", function (req, res) {
-	sqlDb("curriculums").where("doctor", req.params.doctor_id).then(result => {
-		if (result.length === 0) {
-			res.json({
-				error: "Invalid doctor ID"
+function getPrevNext(req, res, next) {
+	let query = sqlDb("doctors");
+	query = filterDoctor(req, query);
+	query.orderBy("surname", "asc").orderBy("name", "asc")
+		.then(result => {
+			let i = _.findIndex(result, (r) => {
+				return req.params.doctor_id == r.id;
 			});
-		} else {
-			res.json(result[0]);
-		}
-	});
-});
+			if (i === -1) {
+				res.json({
+					error: "No doctor found for the given parameters."
+				});
+			} else {
+				let diff = next ? 1 : -1;
+				let doc = result[(i + result.length + diff) % result.length];
+				doc.img = makeURLsAbsolute(doc.img, true);
+				res.json(doc);
+			}
+		});
+}
 
 /* =========================================================
  Services APIs
 ========================================================== */
 
+/* Returns a JSON composed by an array of objects 
+Each object is a distinct Service and contains all the characteristic parameters.
+Filtering is supported, parameters:
+- filter: the type of the filter, it can be equal to area|location
+- value: the ID of an 'object' of the same type specified by the filter parameter 
+		(e.g the ID of the area you may want to know which services belong to)
+Services are ordered by id. */
 app.get("/services", function (req, res) {
 	let filter = _.get(req, "query.filter", null);
 	let value = _.get(req, "query.value", null);
@@ -151,6 +185,8 @@ app.get("/services", function (req, res) {
 	});
 });
 
+/* Returns a JSON object that corresponds to the service specified by "service_id" and all his information.
+If the service ID is invalid, a JSON object containing an error message is returned. */
 app.get("/services/:service_id", function (req, res) {
 	sqlDb("services")
 		.select("services.*", "doctors.id as responsible")
@@ -171,6 +207,14 @@ app.get("/services/:service_id", function (req, res) {
  Areas APIs
 ========================================================== */
 
+/* Returns a JSON composed by an array of objects 
+Each object is a distinct Area and contains all the characteristic parameters.
+Filtering is supported, altough only filter by area is implemented.
+Parameters:
+- filter: the type of the filter, in this case it can only be equal to area (or null)
+- value: the ID of an 'object' of the same type specified by the filter parameter 
+		(e.g the ID of the location you may want to know which areas serves)
+Areas are ordered by ID.*/
 app.get("/areas", function (req, res) {
 	let filter = _.get(req, "query.filter", null);
 	let value = _.get(req, "query.value", null);
@@ -195,6 +239,8 @@ app.get("/areas", function (req, res) {
 		});
 });
 
+/* Returns a JSON object that corresponds to the area specified by "area_id" and all his information.
+If the area ID is invalid, a JSON object containing an error message is returned. */
 app.get("/areas/:area_id", function (req, res) {
 	sqlDb("areas")
 		.where("id", req.params.area_id)
@@ -209,17 +255,26 @@ app.get("/areas/:area_id", function (req, res) {
 		});
 });
 
+
 /* =========================================================
  Location APIs
 ========================================================== */
 
+/* Returns a JSON composed by an array of objects 
+Each object is a distinct Location and contains all the characteristic parameters.
+Filtering is supported, altough only filter by service is implemented.
+Parameters:
+- filter: the type of the filter, in this case can only be equal to "service" (or null)
+- value: the ID of an 'object' of the same type specified by the filter parameter 
+		(e.g the ID of the service you may want to know in which locations is served)
+Locations are ordered by city name */
 app.get("/locations", function (req, res) {
 	let filter = _.get(req, "query.filter", null);
 	let value = _.get(req, "query.value", null);
 	let query = sqlDb("locations");
 	if (filter === null) {
 		switch (filter) {
-			case "location":
+			case "service":
 				query
 					.select("locations.*")
 					.join("servicelocation", "location.id", "servicelocation.location")
@@ -235,6 +290,8 @@ app.get("/locations", function (req, res) {
 	});
 });
 
+/* Returns a JSON object that corresponds to a Location and contains all the characteristic parameters. 
+A JSON object containing an error message is returned*/
 app.get("/locations/:location_id", function (req, res) {
 	sqlDb("locations")
 		.where('id', req.params.location_id)
@@ -273,10 +330,12 @@ app.get("/locations/:location_id/directions", function (req, res) {
 		})
 });
 
-/* =========================================================
- About us APIs
-========================================================== */
 
+/* =========================================================
+ About us API
+ Returns a JSON composed by an array of objects. 
+ Each object contains the title and the corresponding information 
+========================================================== */
 app.get("/aboutus", function (req, res) {
 	sqlDb('whoweare').orderBy("id", "asc").then(result => {
 		res.json(result);
@@ -285,6 +344,8 @@ app.get("/aboutus", function (req, res) {
 
 /* =========================================================
  Home APIs
+ Returns a JSON object that contains the absolute paths to the images that can be used in the carousel,
+ and the information and the url for the information DIVs that are shown in the homepage of the website.
 ========================================================== */
 app.get("/home", function (req, res) {
 	sqlDb("homeslider")
@@ -303,13 +364,9 @@ app.get("/home", function (req, res) {
 					result.map(o => {
 						o.path = makeURLsAbsolute(o.path, false)
 					});
-					home.divs=result;
+					home.divs = result;
 					res.json(home);
 				})
 		});
 });
 
-function makeURLsAbsolute(path, img) {
-	let URL = (DEV ? serverUrl[0] : serverUrl[1]) + (img ? "/assets/img/" : "");
-	return URL + path;
-}
